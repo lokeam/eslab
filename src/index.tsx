33f1432bf9f9bdbfff3f2905072604a53dcde9c8
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-02 18:26:36 -0500

    feat: install esbuild web assembly dependency + copy binary to public folder for future use

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Hello world</h1>
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  }
+
+  return (
+    <div>
+      <textarea 
+        onChange={(event) => setInput(event.target.value)}
+        value={input}
+      >
+      </textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
 };
 
 ReactDOM.render(
