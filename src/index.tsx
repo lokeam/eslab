334700ed2fbeaeb0f192415d09e05fa216e47b9c
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-04 15:25:53 -0500

    fix: reset contents of iframe before execution and remove extra console warnings

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,6 +23,9 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
+    // Todo: clean this area up
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -38,9 +40,6 @@ const App = () => {
       },
     });
 
-    console.log(result);
-
-    // setCode(result.outputFiles[0].text);
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   }
 
@@ -74,8 +73,11 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html} />
     </div>
   )
 };
