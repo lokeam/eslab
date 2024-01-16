35cb67b198fe8fc596bbb20d0e13d503694d5f25
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-16 15:54:26 -0500

    fix: resolve preview window flash add loading message

--- src/components/code-box.tsx
+++ src/components/code-box.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { Box } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import './code-box.css';
 
 interface CodeBoxProps {
   box: Box
@@ -15,6 +16,11 @@ const CodeBox: React.FC<CodeBoxProps> = ({ box }) => {
   const bundle = useTypedSelector((state) => state.bundles[box.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(box.id, box.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(box.id, box.content)
     }, 750);
@@ -22,19 +28,30 @@ const CodeBox: React.FC<CodeBoxProps> = ({ box }) => {
     return () => {
       clearTimeout(timer);
     };
+    // Todo: Find better way to do remove output window flash without
+    // including bundle pointer into dependency array
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [box.id, box.content, createBundle]);
 
   return (
-    <Resizable direction='vertical'>
-      <div style={{height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row'}}>
-        <Resizable direction='horizontal'>
+    <Resizable direction="vertical">
+      <div style={{height: "calc(100% - 10px)", display: "flex", flexDirection: "row"}}>
+        <Resizable direction="horizontal">
           <CodeEditor
             initialValue={box.content}
             onChange={(value) => updateBox(box.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} /> }
-    </div>
+        <div className="progress-cover-wrapper">
+          {!bundle || bundle.processing ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">Loading</progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
+        </div>
+      </div>
     </Resizable>
   )
 };
